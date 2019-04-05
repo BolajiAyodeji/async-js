a6343375b8294d82c5527ae5126400302966a251
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-05 06:28:19 +0100

    listing

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
    <link rel="shortcut icon" href="./.favicon.ico">
    <title>Directory Contents</title>
 
-   <link rel="stylesheet" href="./.style.css">
-   <script src="./.sorttable.js"></script>
+   <link rel="stylesheet" href="./style.css">
+   <script src="./sorttable.js"></script>
 </head>
 
 <body>
