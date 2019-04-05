cc8fc9b8ea515607f99868249ea9377f22fbfd48
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-05 13:01:53 +0100

    bajax

--- 06-Custom HTTP Library/index.js
+++ 06-Custom HTTP Library/index.js
@@ -1,12 +1,48 @@
 
 const http = new bAJAX;
 
-// GET posts
+const data = {
+  userId: 101,
+  title: 'Custom post',
+  body: 'This is a custom post'
+};
+
+// GET request
 posts = http.get('https://jsonplaceholder.typicode.com/posts',
-function(err, posts) {
+(err, posts) => {
   if(err) {
     console.log(err)
   } else {
   console.log(posts);
+  }
+});
+
+// POST request
+http.post('https://jsonplaceholder.typicode.com/posts',
+data, (err, post) => {
+  if(err) {
+    console.log(err)
+  } else {
+    console.log(post);
+  }
+});
+
+// PUT request
+http.put('https://jsonplaceholder.typicode.com/posts/1',
+data, (err, post) => {
+  if(err) {
+    console.log(err)
+  } else {
+    console.log(post);
+  }
+});
+
+// DELETE request
+posts = http.delete('https://jsonplaceholder.typicode.com/posts/1',
+(err, response) => {
+  if(err) {
+    console.log(err)
+  } else {
+  console.log(response);
   }
 });
